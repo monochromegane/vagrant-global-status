88790551c68cbe2f112ad56943cceac721f0972f
monochromegane <dev.kuro.obi@gmail.com>
2014-09-06 08:37:39 +0900

    Moved types to other file.

--- status.go
+++ status.go
@@ -9,32 +9,6 @@ import (
 	"regexp"
 )
 
-type MachineIndex struct {
-	Version  int                `json:"version"`
-	Machines map[string]Machine `json:"machines"`
-}
-
-type Machine struct {
-	LocalDataPath   string    `json:"local_data_path"`
-	Name            string    `json:"name"`
-	Provider        string    `json:"provider"`
-	State           string    `json:"state"`
-	VagrantfileName string    `json"vagrantfile_name"`
-	VagrantfilePath string    `json:"vagrantfile_path"`
-	UpdatedAt       string    `json:"updated_at"`
-	ExtraData       ExtraData `json:"extra_data"`
-}
-
-type ExtraData struct {
-	Box Box `json:"box"`
-}
-
-type Box struct {
-	Name     string `json:"name"`
-	Provider string `json:"provider"`
-	Version  string `json:"version"`
-}
-
 func GlobalStatus() {
 	usr, _ := user.Current()
 	homeDir := usr.HomeDir
