72c38334f816c9793b4a0595507b97e0a03d4d61
monochromegane <dev.kuro.obi@gmail.com>
2014-09-06 08:12:34 +0900

    Parse machine-index-file.

--- status.go
+++ status.go
@@ -1,11 +1,38 @@
 package vagrant
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os/user"
 	"path/filepath"
 )
+
+type MachineIndex struct {
+	Version  int                `json:"version"`
+	Machines map[string]Machine `json:"machines"`
+}
+
+type Machine struct {
+	LocalDataPath   string    `json:"local_data_path"`
+	Name            string    `json:"name"`
+	Provider        string    `json:"provider"`
+	State           string    `json:"state"`
+	VagrantfileName string    `json"vagrantfile_name"`
+	VagrantfilePath string    `json:"vagrantfile_path"`
+	UpdatedAt       string    `json:"updated_at"`
+	ExtraData       ExtraData `json:"extra_data"`
+}
+
+type ExtraData struct {
+	Box Box `json:"box"`
+}
+
+type Box struct {
+	Name     string `json:"name"`
+	Provider string `json:"provider"`
+	Version  string `json:"version"`
+}
 
 func GlobalStatus() {
 	usr, _ := user.Current()
@@ -18,4 +45,8 @@ func GlobalStatus() {
 		return
 	}
 	fmt.Printf("%s\n", string(file))
+
+	var machineIndex MachineIndex
+	json.Unmarshal(file, &machineIndex)
+	fmt.Printf("Results: %v\n", machineIndex)
 }
